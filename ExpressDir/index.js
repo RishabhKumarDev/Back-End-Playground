1d987a0309c9d7ebc5c8ba652c84fa68fda39ec0
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-08-10 14:56:59 +0530

    Installed Nodemon dynamic Routes query string

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -6,27 +6,49 @@ app.listen(port, () => {
   console.log(`hello:: Express is working on the Port ${port}`);
 });
 
+app.get("/", (req, res) => {
+  console.log("in root ");
 
-app.get("/",(req,res)=>{
-    console.log("in root ");
-    
-    res.send("response sent from root ")
-})
+  res.send("HI i am  root ");
+});
 
-app.get("/apple",(req,res)=>{
-    console.log("in apple ");
+// Query String
+app.get("/search", (req, res) => {
+  console.log("received query", req.query);
+  let {q} = req.query;
 
-    res.send("response sent from apple ")
-})
-
-
-app.get("/banana",(req,res)=>{
-    console.log("in banana ");
-
-    res.send("response sent from banana ")
-})
+  res.send(`Query Result are: ${q} `);
+});
 
 
+
+// // dynamic path(params);
+// app.get("/:username/:userId",(req,res)=>{
+
+//     let {username, userId} = req.params;
+
+//     res.send(`hi dude your username is ${username +" " + userId  }`)
+// })
+
+// app.get("/:username/:userId/:oneId",(req,res)=>{
+
+//     let {username, userId, oneId} = req.params;
+
+//     res.send(`hi dude your username is ${username +" " + userId + "  " + oneId }`)
+// })
+
+// response depending on Request
+// app.get("/apple",(req,res)=>{
+//     console.log("in apple ");
+
+//     res.send("response sent from apple ")
+// })
+
+// app.get("/banana",(req,res)=>{
+//     console.log("in banana ");
+
+//     res.send("response sent from banana ")
+// })
 
 // app.use((req, res) => {
 
