be058d87e034e306f68d872e47d32d8abecee6a6
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-08-11 18:07:22 +0530

    Today's Topics That i deep dived into:-- Interpolation Syntax. Passing Data to EJS. IG EJS(practice) Conditional Rendering in EJS Loops in EJS working with more data in json format and different layout conditional rendering Serving static files path decleration for static files

--- EJSdir/index.js
+++ EJSdir/index.js
@@ -6,10 +6,32 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+// serving static files
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/js")));
 
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/rolldice", (req, res) => {
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice", { diceVal });
+});
+
+app.get("/ig/:username", (req, res) => {
+  // let followers = ["abc", "kanab", "sumit", "anna" , "panna", "sanna"];
+  let { username } = req.params;
+
+  const instaData = require("./data.json");
+  const data = instaData[username];
+
+  if (data) {
+    res.render("instagram", { ...data });
+  } else {
+    res.render("error");
+  }
 });
 
 app.get("/hi", (req, res) => {
