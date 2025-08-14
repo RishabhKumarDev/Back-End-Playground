6a4b555086e53bf734b91bc4e1adfe612a09c1aa
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-08-14 12:13:00 +0530

    OOPs, Class, Inheritance, Prototypes

--- miscellaneous/Frontend/app.js
+++ miscellaneous/Frontend/app.js
@@ -1,42 +1,29 @@
-// first way -- Factory function
-function personMaker(name, age) {
-  const person = {
-    name,
-    age,
-    talk() {
-      console.log(`hii my name is ${this.name}`);
-    },
-  };
-
-  return person;
-}
-// each obj will take space for every talk method
-const p1 = personMaker("rahu", 45000);
-const p2 = personMaker("katu", 45000);
-
-// second way :== Constructors function { convension :- starts with Capital letter && doesn't return anything}
-
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-Person.prototype.talk = function () {
-  console.log(`hii i am ${this.name}`);
-};
-
-const pp1 = new Person("ati", 23);
-const pp2 = new Person("andf", 20);
-
-// third way:- Using classes and constructor method (special methods for classes);
-
 class Person {
   constructor(name, age) {
+    console.log("this will be logged 2");
+
     this.name = name;
     this.age = age;
   }
 
   talk() {
-    console.log(`hi i am ${this.name} and i got created using a classes`);
+    console.log(`hey, my name is ${this.name}`);
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, marks) {
+    console.log("this will be logged 1");
+
+    super(name, age);
+    console.log("this will be logged 3");
+
+    this.marks = marks;
+  }
+}
+class Teacher extends Person {
+  constructor(name, age, subject) {
+    super(name, age);
+    this.subject = subject;
   }
 }
