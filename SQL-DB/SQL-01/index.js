6d256a3198324476eb4cf817dc13d78da937874d
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-08-21 20:55:11 +0530

    Insearting values in db in bulk using faker;

--- SQL-DB/SQL-01/index.js
+++ SQL-DB/SQL-01/index.js
@@ -9,20 +9,29 @@ const mysql = require("mysql2/promise");
     database: "delta_app",
     password: "3EvdK%yR05$",
   });
-  let name;
+
   const q = "INSERT INTO user(id,username, email, password) VALUES ?";
   // insearting one value (?,?,?,?);
   //   const values = ["001", "001_hibkl", "haahoo@gmail.com", "123456n"];
 
   // inserting multiple values
-  const users = [
-    ["002", "001_hibklb", "haahoob@gmail.com", "123456nb"],
-    ["003", "001_hibklc", "haahooc@gmail.com", "123456nc"],
-    ["004", "001_hibkld", "haahood@gmail.com", "123456nd"],
-  ];
+  // const users = [
+  //   ["002", "001_hibklb", "haahoob@gmail.com", "123456nb"],
+  //   ["003", "001_hibklc", "haahooc@gmail.com", "123456nc"],
+  //   ["004", "001_hibkld", "haahood@gmail.com", "123456nd"],
+  // ];
+
+  // inserting bulk data at once;
+
+  const data = [];
+  for (let i = 1; i <= 100; i++) {
+    data.push(getRandomUser());
+  }
+ console.log(data);
 
   try {
-    const [result] = await connection.query(q, [users]);
+    const [result] = await connection.query(q, [data]);
+    
 
     console.log(result);
   } catch (error) {
@@ -33,12 +42,10 @@ const mysql = require("mysql2/promise");
 })();
 
 const getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+  return [
+    faker.string.uuid(),
+    faker.internet.username(), // before version 9.1.0, use userName()
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
 };
-
-// console.log(getRandomUser());
