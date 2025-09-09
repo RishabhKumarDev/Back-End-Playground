cdbcc08eb8a118cdc396688edb1cb9774b448354
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-09-09 22:10:48 +0530

    mongoose middlewares in depth;

--- MongoDB/RelationShips/models/customer.js
+++ MongoDB/RelationShips/models/customer.js
@@ -25,16 +25,30 @@ const customerSchema = new Schema({
   ],
 });
 
+customerSchema.pre("findOneAndDelete", function () {
+  console.log("PRE MIDDLEWARE");
+});
+
+customerSchema.post("findOneAndDelete", async function (customer, next) {
+  if (!customer.orders.length) return next();
+  const deleteOrders = await Order.deleteMany({
+    _id: { $in: customer.orders },
+  });
+
+  console.log(deleteOrders);
+  console.log("POST MIDDLEWARE");
+});
+
 const Order = mongoose.model("Order", orderSchema);
 const Customer = mongoose.model("Customer", customerSchema);
 
 const addCustomer = async () => {
   const customer1 = new Customer({
-    name: "haddi kala",
+    name: "karan",
   });
 
-  let order1 = await Order.findOne({ item: "sofa" });
-  let order2 = await Order.findOne({ item: "drone" });
+  let order1 = await Order.findOne({ item: "pizza" });
+  let order2 = await Order.findOne({ item: "samosa" });
 
   customer1.orders.push(order1);
   customer1.orders.push(order2);
@@ -46,27 +60,30 @@ const addCustomer = async () => {
 const addOrder = async () => {
   const result = await Order.insertMany([
     {
-      item: "phone",
-      price: 49999,
+      item: "pizza",
+      price: 499,
     },
     {
-      item: "drone",
-      price: 12999,
-    },
-    {
-      item: "sofa",
-      price: 29999,
+      item: "samosa",
+      price: 12,
     },
   ]);
 
   console.log(result);
 };
- 
-const findCustomer = async()=>{
-    const result = await Customer.findOne({}).populate("orders");
-    console.log(result);
-}
+
+const findCustomer = async () => {
+  const result = await Customer.findOne({ name: "karan" }).populate("orders");
+  console.log(result);
+};
+
+const deleteCustomer = async () => {
+  const deletedCustomer = await Customer.findByIdAndDelete(
+    "68bfc784ed21fc655ce5f00b"
+  );
+};
 
 // addOrder();
 // addCustomer();
-findCustomer();
+// findCustomer();
+deleteCustomer();
